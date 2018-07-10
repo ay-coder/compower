846b8ba0e371054e4efc7622b2036250801c9c7a
Anuj Jaha <er.anujjaha@gmail.com>
2018-07-10 11:30:48 +0530

    Updated Hash

--- app/Library/Push/PushNotification.php
+++ app/Library/Push/PushNotification.php
@@ -128,7 +128,10 @@ class PushNotification
 		$payload = json_encode($body);
 
 		// Build the binary notification
-		$msg = chr(0) . pack('n', 32) . strtr(rtrim(base64_encode(pack('H*', sprintf('%u', CRC32($deviceToken)))) . pack('n', strlen($payload)) . $payload;
+		$msg = chr(0) . pack('n', 32) . strtr(rtrim(base64_encode(pack('H*', sprintf('%u', CRC32($deviceToken)))))) . pack('n', strlen($payload)) . $payload;
+
+
+		
 
 		// Send it to the server
 		$result = fwrite($fp, $msg, strlen($msg));
